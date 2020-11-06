5299eccc8a6aba9576b2a5ecad1963a1b592c364
juanmarcosdev <juanmarcoscaicedo@gmail.com>
2020-11-06 15:28:23 -0500

    Obteniendo tablero

--- main.cpp
+++ main.cpp
@@ -10,13 +10,14 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    int largoTablero = 0;
-    int anchoTablero = 0;
+    int filas = 0;
+    int columnas = 0;
+    int datos = 0;
     ifstream file(argv[1]);
     if(file.is_open()) {
         string line;
         getline(file, line);
-        anchoTablero = line.length();
+        columnas = line.length();
         file.close();
     }
     ifstream file2(argv[1]);
@@ -25,11 +26,42 @@ int main(int argc, char* argv[])
         while(getline(file2, line)) {
             if(line.length() != 3)
             {
-                largoTablero++;
+                filas++;
             }
+            if(line.length() == 3)
+            {
+                datos++;
+            } 
         }
         file.close();
     }
-    cout << largoTablero << endl;
-    cout << anchoTablero << endl;
+    cout << filas << endl;
+    cout << columnas << endl;
+    cout << datos << endl;
+    char tablero[filas][columnas];
+    int coordenadas[datos][2];
+    ifstream file3(argv[1]);
+    int contador = 0;
+    if(file3.is_open()) {
+        string line;
+        while(getline(file3, line)) {
+            if(line.length() != 3)
+            {
+                for(int i = 0; i < line.length(); i++)
+                {
+                    tablero[contador][i] = line[i];
+                } 
+                contador++;
+            }
+        }
+        file3.close();
+    }
+    for(int i = 0; i < filas; i++)
+    {
+        for(int j = 0; j < columnas; j++)
+        {
+            cout << tablero[i][j];
+        }
+        cout << endl;
+    }
 }
