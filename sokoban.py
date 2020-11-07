0eb7661f654d1b775f6344550e2a5f2cb38c38fe
juanmarcosdev <juanmarcoscaicedo@gmail.com>
2020-11-07 00:18:00 -0500

    Configuring

--- sokoban.py
+++ sokoban.py
@@ -18,7 +18,7 @@ def leerArchivo():
     for i in range(0,len(Lines)):
         if(Lines[i][0] == 'W' or Lines[i][0] == '0'):
             filas = filas + 1
-            tablero.append([Lines[i]])
+            tablero.append(Lines[i])
         if(Lines[i][0] != 'W' and Lines[i][0] != '0' and contador != 0):
             ubicaciones_cajas.append([int(Lines[i][0]), int(Lines[i][2])])
         if(Lines[i][0] != 'W' and Lines[i][0] != '0' and contador == 0):
@@ -28,10 +28,51 @@ def leerArchivo():
 
     columnas = len(Lines[0])
 
-    print(filas)
-    print(columnas)
-    print(posicion)
-    print(ubicaciones_cajas)
-    print(tablero)
+    return filas, columnas, posicion, ubicaciones_cajas, tablero
 
-leerArchivo()
+class State:
+    def __init__(self, filas, columnas, posicion, ubicaciones_cajas, tablero):
+        self.filas = filas
+        self.columnas = columnas
+        self.posicion = posicion
+        self.ubicaciones_cajas = ubicaciones_cajas
+        self.tablero = tablero
+        self.movimientos = []
+    
+    def jugadasValidas(self):
+        jugadas_validas = []
+
+        if(self.tablero[self.posicion[0]-1][self.posicion[1]] != 'W'):
+            jugadas_validas.append('U')
+        if(self.tablero[self.posicion[0]+1][self.posicion[1]] != 'W'):
+            jugadas_validas.append('D')
+        if(self.tablero[self.posicion[0]][self.posicion[1]-1] != 'W'):
+            jugadas_validas.append('L')
+        if(self.tablero[self.posicion[0]][self.posicion[1]+1] != 'W'):
+            jugadas_validas.append('R')
+        
+        return jugadas_validas
+    
+    def lugaresCajaIdeales(self):
+        lugares_ideales = []
+        for i in range(0,self.filas):
+            for j in range(0,self.columnas):
+                if(self.tablero[i][j] == 'X'):
+                    lugares_ideales.append([i,j])
+        return lugares_ideales
+    
+    def ganeElJuego(self):
+        return self.ubicaciones_cajas == self.lugaresCajaIdeales()
+    
+    def nuevoEstado(self, movimiento):
+        if(movimiento == 'U'):
+            
+
+rows, columns, position, boxes_positions, board = leerArchivo()
+
+initialState = State(rows, columns, position, boxes_positions, board)
+
+print(initialState.jugadasValidas())
+print(initialState.ubicaciones_cajas)
+print(initialState.lugaresCajaIdeales())
+print(initialState.ganeElJuego())
